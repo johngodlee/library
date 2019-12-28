06412527f3bd99cb653bc7f11d46882e12942a16
johngodlee <johngodlee@gmail.com>
2019-12-28 21:58:18 +0000

    Updated compile.sh

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ latexmk -pdf books.tex
 
 pandoc --filter pandoc-citeproc --csl /Users/johngodlee/.texmf/apa.csl --standalone books.md -o books.html
 
-sed -n "20,132p" books.html | pandoc -f html -t markdown_strict -o index.md
+sed -n "20,860p" books.html | pandoc -f html -t markdown_strict -o index.md
 
 pandoc -f html -t plain --columns=68 -o books.txt books.html
 
